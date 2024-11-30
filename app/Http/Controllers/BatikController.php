32026e417b894fc91f06cadb5c5ad4550f40d50c
Naufal Haris <naufal.michiels@gmail.com>
2024-11-30 15:44:04 +0700

    feat(batik): admin can create, update, and delete batik

--- app/Http/Controllers/BatikController.php
+++ app/Http/Controllers/BatikController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Batik;
+use App\Services\SupabaseService;
 use Illuminate\Support\Facades\DB;
 
 class BatikController extends Controller
@@ -24,14 +25,14 @@ class BatikController extends Controller
         ]);
     }
 
-   public function tes()
-{
-    $batiks = Batik::all();
+    public function tes()
+    {
+        $batiks = Batik::all();
 
-    return Inertia::render('Catalog', [
-        'batiks' => $batiks,
-    ]);
-}
+        return Inertia::render('Catalog', [
+            'batiks' => $batiks,
+        ]);
+    }
 
     public function store(Request $request)
     {
@@ -47,56 +48,56 @@ class BatikController extends Controller
     }
 
     public function catalog(Request $request)
-{
-    $pulau = $request->input('pulau');
-    $provinsi = $request->input('provinsi');
-    $page = $request->input('page', 1);
-    
+    {
+        $pulau = $request->input('pulau');
+        $provinsi = $request->input('provinsi');
+        $page = $request->input('page', 1);
 
-    $limit = 9;
-    $offset = ($page - 1) * $limit;
 
-    $query = DB::table('batiks')
-        ->join('provinces', 'batiks.provinceId', '=', 'provinces.id')
-        ->join('islands', 'batiks.islandId', '=', 'islands.id')
-        ->select(
-            'batiks.*',
-            'provinces.name as province_name',
-            'islands.name as island_name'
-        );
+        $limit = 9;
+        $offset = ($page - 1) * $limit;
 
-    if (!is_null($pulau)) {
-        $query->where('batiks.islandId', $pulau);
+        $query = DB::table('batiks')
+            ->join('provinces', 'batiks.provinceId', '=', 'provinces.id')
+            ->join('islands', 'batiks.islandId', '=', 'islands.id')
+            ->select(
+                'batiks.*',
+                'provinces.name as province_name',
+                'islands.name as island_name'
+            );
+
+        if (!is_null($pulau)) {
+            $query->where('batiks.islandId', $pulau);
+        }
+
+        if (!is_null($provinsi)) {
+            $query->where('batiks.provinceId', $provinsi);
+        }
+
+        $total = $query->count();
+        $batik = $query->offset($offset)->limit($limit)->get();
+
+        $islands = DB::table('islands')->get();
+        $provinces = DB::table('provinces')->get();
+
+        return Inertia::render('Catalog', [
+            'batik' => $batik,
+            'islands' => $islands,
+            'provinces' => $provinces,
+            'currentPage' => (int)$page,
+            'totalPages' => (int) ceil($total / $limit),
+            'selectedIsland' => $pulau,
+            'selectedProvince' => $provinsi,
+        ]);
     }
-
-    if (!is_null($provinsi)) {
-        $query->where('batiks.provinceId', $provinsi);
-    }
-
-    $total = $query->count();
-    $batik = $query->offset($offset)->limit($limit)->get();
-
-    $islands = DB::table('islands')->get();
-    $provinces = DB::table('provinces')->get();
-
-    return Inertia::render('Catalog', [
-        'batik' => $batik,
-        'islands' => $islands,
-        'provinces' => $provinces,
-        'currentPage' => (int)$page,
-        'totalPages' => (int) ceil($total / $limit),
-        'selectedIsland' => $pulau,
-        'selectedProvince' => $provinsi,
-    ]);
-}
 
     public function overview($id)
     {
         $batik = DB::table('batiks')
-        ->join('provinces', 'batiks.provinceId', '=', 'provinces.id')
-        ->select('batiks.*', 'provinces.name as province_name') 
-        ->where('batiks.id', $id)
-        ->first();
+            ->join('provinces', 'batiks.provinceId', '=', 'provinces.id')
+            ->select('batiks.*', 'provinces.name as province_name')
+            ->where('batiks.id', $id)
+            ->first();
 
 
         if (!$batik) {
@@ -115,5 +116,76 @@ class BatikController extends Controller
             'batik' => $batik,
             'relatedBatik' => $relatedBatik,
         ]);
+    }
+
+    public function create(Request $request, SupabaseService $supabaseService)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'islandId' => 'required|integer',
+            'city' => 'required|string',
+            'tag' => 'nullable|string',
+            'provinceId' => 'required|integer',
+            'image' => 'required|file|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $imagePath = $request->file('image')->getPathname();
+        $imageName = $request->file('image')->getClientOriginalName();
+        $imageUrl = $supabaseService->uploadFile($imagePath, $imageName);
+
+        $batik = Batik::create([
+            'name' => $validated['name'],
+            'description' => $validated['description'],
+            'islandId' => $validated['islandId'],
+            'city' => $validated['city'],
+            'tag' => $validated['tag'],
+            'provinceId' => $validated['provinceId'],
+            'linkImage' => $imageUrl,
+        ]);
+
+        return Inertia::render('Admin');
+    }
+
+    function delete($id)
+    {
+        $batik = Batik::find($id);
+        $batik->delete();
+
+        return Inertia::render('Admin');
+    }
+
+    public function update(Request $request, SupabaseService $supabaseService, $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'islandId' => 'required|integer',
+            'city' => 'required|string',
+            'tag' => 'nullable|string',
+            'provinceId' => 'required|integer',
+            'image' => 'nullable|file|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $batik = Batik::findOrFail($id);
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->getPathname();
+            $imageName = $request->file('image')->getClientOriginalName();
+            $imageUrl = $supabaseService->uploadFile($imagePath, $imageName);
+
+            $batik->linkImage = $imageUrl;
+        }
+
+        $batik->name = $validated['name'];
+        $batik->description = $validated['description'];
+        $batik->islandId = $validated['islandId'];
+        $batik->city = $validated['city'];
+        $batik->tag = $validated['tag'];
+        $batik->provinceId = $validated['provinceId'];
+
+        $batik->save();
+
+        return Inertia::render('Admin');
     }
 }
